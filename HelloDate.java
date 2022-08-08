204a912b0e9be83b0607a0daeb2119a01e9b481c
Yevgeniy Yuzhaninov <jahdao@gmail.com>
2022-08-08 13:22:43 +0700

    Init commit

---
+++ HelloDate.java
@@ -0,0 +1,13 @@
+// HelloDate.java
+package com.huobi.prj;
+import java.util.*;
+import com.huobi.prj.MyClass;
+
+public class HelloDate {
+    private byte[] byteArr;
+    public static void main(String[] args) {
+        System.out.println("Hello. it's: ");
+        System.out.println(new Date());
+        teh = new com.huobi.prj.MyClass();
+    }
+}
---
+++ MyClass.java
@@ -0,0 +1,12 @@
+package com.huobi.prj;
+
+class MyClass {
+    private int i;
+    private String s;
+    public void setI(int i){
+        this.i = i;
+    }
+    public byte[] getS(){
+        return s.getBytes();
+    }
+}
