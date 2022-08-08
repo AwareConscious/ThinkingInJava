e505ce0e9770c0acbe7734207374e0ff62693ea8
Yevgeniy Yuzhaninov <jahdao@gmail.com>
2022-08-08 14:07:49 +0700

    new string

--- HelloDate.java
+++ HelloDate.java
@@ -5,9 +5,11 @@ import com.huobi.prj.MyClass;
 
 public class HelloDate {
     private byte[] byteArr;
+    private String name;
     public static void main(String[] args) {
         System.out.println("Hello. it's: ");
         System.out.println(new Date());
         the = new com.huobi.prj.MyClass();
+
     }
 }
