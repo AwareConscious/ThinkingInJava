110654f1172739a0c611cab19cd9711182f7001c
Yevgeniy Yuzhaninov <jahdao@gmail.com>
2022-08-09 19:30:06 +0700

    Next

--- HelloDate.java
+++ HelloDate.java
@@ -1,15 +1,9 @@
 // HelloDate.java
 package com.huobi.prj;
 import java.util.*;
-import com.huobi.prj.MyClass;
-
 public class HelloDate {
-    private byte[] byteArr;
-    private String name;
     public static void main(String[] args) {
-        System.out.println("Hello. it's: ");
+        System.out.println("Hello, it's: ");
         System.out.println(new Date());
-        the = new com.huobi.prj.MyClass();
-
     }
 }
